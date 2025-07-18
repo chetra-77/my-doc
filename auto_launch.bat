a5b1e51a2896ab83651f9552f32661c254685c91
chetra-77 <e20201277@itc.edu.kh>
2025-07-18 15:56:18 +0700

    Update auto_launch.bat

--- auto_launch.bat
+++ auto_launch.bat
@@ -1,17 +1,20 @@
 @echo off
 timeout /t 10 /nobreak >nul
 
-REM Open first URL in first browser window
-start "" https://www.google.com
+REM Open first URL
+start "" https://www.example1.com
 
 REM Wait for first browser to fully load
 timeout /t 8 /nobreak >nul
 
-REM Open second URL in NEW browser window
-start "" https://www.github.com
+REM Press Ctrl+Shift+N to force new window, then navigate to second URL
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('^+n'); Start-Sleep -Milliseconds 2000; [System.Windows.Forms.SendKeys]::SendWait('^l'); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait('https://www.example2.com'); [System.Windows.Forms.SendKeys]::SendWait('{ENTER}');"
 
-REM Wait for second browser to load
-timeout /t 3 /nobreak >nul
+REM Wait a moment for second window to load
+timeout /t 5 /nobreak >nul
 
-REM Auto-arrange windows (first window left, second window right)
-powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{LWIN}+{LEFT}'); Start-Sleep -Milliseconds 1000; [System.Windows.Forms.SendKeys]::SendWait('{LWIN}+{RIGHT}');"
+REM Now arrange windows - Win+Left for first window
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('%{TAB}'); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait('{LWIN}+{LEFT}'); Start-Sleep -Milliseconds 1000;"
+
+REM Win+Right for second window
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('%{TAB}'); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait('{LWIN}+{RIGHT}');"
